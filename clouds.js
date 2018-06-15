e1c0b341f0971d329b170983ea47313356a3ce29
Dawiid <David_May@codemao.cn>
2018-06-15 14:06:48 +0800

    setup glslify

--- clouds.js
+++
@@ -1,9 +0,0 @@
-const regl = require('regl')({
-    canvas: document.getElementById('regl-canvas'),
-  });
-  
-  regl.clear({
-    color: [0, 0, 0, 0.5],
-    depth: 1
-  })
- 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import glsl = require('glslify');
+
 const regl = require('regl')({
   canvas: document.getElementById('regl-canvas'),
 });
@@ -8,27 +10,22 @@ regl.clear({
 })
 
 regl({
-  frag: 
-  `
-    precision mediump float;
-    uniform vec4 u_color;
-    void main() {
-      gl_FragColor = u_color;
-    }
-  `,
-  vert:
-  `
-    precision mediump float;
-    attribute vec2 a_position;
-    void main() {
-      gl_Position = vec4(a_position, 0, 1);
-    }
-  `,
-  attributes: {
-    a_position: [[-0.5, 0], [0, -0.5], [0, 0.5], [0.5, 0], [0.2, -0.5], [0.2, 0.5]]
-  },
+  frag: glsl('./gl/sky_frag.glsl'),
+  vert: glsl('./gl/sky_vert.glsl'),
   uniforms: {
-    u_color: [0, 1, 0, 1],
+    viewportHeight: regl.context('viewportHeight'),
+    viewportWidth: regl.context('viewportWidth'),
   },
-  count: 6,
+  attributes: {
+    position:
+      [
+        -4, 0,
+        4, -4,
+        4, 4,
+      ],
+  },
+  count: 3,
+  primitive: 'triangles',
+  offset: 0,
+  elements: null,
 })();
